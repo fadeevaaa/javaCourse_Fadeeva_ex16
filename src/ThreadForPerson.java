9f4cad39efe70892349c3a901f9144e0dbd19a00
fadeevaaa <fadeevaaa29@gmail.com>
2025-02-07 20:43:50 +0400

    javaCourse_Fadeeva_ex16

---
+++ src/ThreadForPerson.java
@@ -0,0 +1,12 @@
+public class ThreadForPerson implements Runnable {
+    private Road road;
+
+    public ThreadForPerson(Road road) {
+        this.road = road;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
